d0128fc35eb8609f77478087b183d4b541a76a0c
FredM7 <fred@moller.cloud>
2020-05-31 15:19:41 +0200

    implemented transactions sample

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 const sha256 = require("crypto-js/sha256");
 const moment = require("moment");
 
+class Transaction {
+    constructor(from, to, amount) {
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
+    }
+}
+
 class Block {
-    constructor(index, timestamp, data, previousHash = "") {
-        this.index = index;
+    constructor(timestamp, transactions, previousHash = "") {
         this.nonce = 0; //This number does not have anything to do with the block, but can be anything random.
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
     }
 
     calculateHash() {
-        return sha256(`${this.nonce}${this.index}${this.previousHash}${this.timestamp}${JSON.stringify(this.data)}`).toString();
+        return sha256(`${this.nonce}${this.previousHash}${this.timestamp}${JSON.stringify(this.data)}`).toString();
     }
 
     mineBlock(difficulty) {
@@ -26,25 +33,67 @@ class Block {
 
 class Blockchain {
     constructor() {
-        this.difficulty = 4;
+        this.difficulty = 2;
         this.chain = [this.createGenesisBlock()];
+        this.pendingTransactions = [];
+        this.miningReward = 1;
     }
 
     createGenesisBlock() {
-        return new Block(0, "01/01/2020", "Genesis block", "0");
+        return new Block(moment().format("YYYY-MM-DD::HH::mm:ss"), "Genesis block", "0");
     }
 
     getLatestBlock() {
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock) {
-        newBlock.previousHash = this.getLatestBlock().hash;
-        // newBlock.hash = newBlock.calculateHash();
-        newBlock.mineBlock(this.difficulty);
-        //Normally in reality you cant add a block so easily.
-        //Because there should be numerous checks in place.
-        this.chain.push(newBlock);
+    // addBlock(newBlock) {
+    //     newBlock.previousHash = this.getLatestBlock().hash;
+    //     // newBlock.hash = newBlock.calculateHash();
+    //     newBlock.mineBlock(this.difficulty);
+    //     //Normally in reality you cant add a block so easily.
+    //     //Because there should be numerous checks in place.
+    //     this.chain.push(newBlock);
+    // }
+    minePendingTransactions(minerAddress) {
+        //In real world cryptocurrencies, adding all pending transactions to a block
+        //is impossible because there are way too many transactions.
+        //The block size should also probably not surpass +- 1MB...
+        //So, miners really get to choose which transactions they include and which they dont.
+        let block = new Block(moment().format("YYYY-MM-DD::HH::mm:ss"), this.pendingTransactions);
+        block.mineBlock(this.difficulty);
+        console.log("Block successfully mined!");
+        this.chain.push(block);
+
+        this.pendingTransactions = [
+            new Transaction(null, minerAddress, this.miningReward)
+        ];
+
+        //Now, we can really change the code adn givce ourselves more coins,
+        //but cryprocurrencies are powered by a peer-peer network,
+        //and other nodes won't accept the change and ignore you.
+    }
+
+    createTransaction(transaction) {
+        this.pendingTransactions.push(transaction);
+    }
+
+    getAddressBalance(address) {
+        let balance = 0;
+
+        for (const block of this.chain) {
+            for (const trans of block.transactions) {
+                if (trans.from === address) {
+                    balance -= trans.amount;
+                }
+
+                if (trans.to === address) {
+                    balance += trans.amount;
+                }
+            }
+        }
+
+        return balance;
     }
 
     verifyIntegrity() {
@@ -69,14 +118,15 @@ class Blockchain {
 }
 
 let my_coin = new Blockchain();
-// my_coin.addBlock(new Block(1, "01/02/2020", { amount: 4 }));
-// my_coin.addBlock(new Block(2, "01/03/2020", { amount: 10 }));
-for (let i = 0; i < 10; i++) {
-    my_coin.addBlock(new Block(i, moment().format("YYYY-MM-DD::HH::mm:ss"), { amount: 4 }));
-}
+my_coin.createTransaction(new Transaction("1", "2", 10));
+my_coin.createTransaction(new Transaction("2", "1", 3));
 
-console.log(JSON.stringify(my_coin, null, 4));
-// console.log("Is chain valid?", my_coin.verifyIntegrity());
-// //my_coin.addBlock(new Block({ amount: 11 }));
-// my_coin.chain[1].data = { amount: 100 };
-// console.log("Is chain valid?", my_coin.verifyIntegrity());
+console.log("Starting the mining process...");
+my_coin.minePendingTransactions("3");
+console.log("3's balance is:", my_coin.getAddressBalance("3"));
+
+console.log("Starting the mining process...");
+my_coin.minePendingTransactions("3");
+console.log("3's balance is:", my_coin.getAddressBalance("3"));
+console.log("2's balance is:", my_coin.getAddressBalance("2"));
+console.log("1's balance is:", my_coin.getAddressBalance("1"));
